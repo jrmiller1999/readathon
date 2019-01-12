3aafe593a8a3545b69d74b75765a0906df412de1
root <root@li434-221.members.linode.com>
2019-01-12 04:12:59 +0000

    minor updates for 2019 to hello_world.py

--- setup_db.sql
+++ setup_db.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.73, for redhat-linux-gnu (i386)
+-- MySQL dump 10.14  Distrib 5.5.60-MariaDB, for Linux (x86_64)
 --
--- Host: localhost    Database: readathon2015
+-- Host: localhost    Database: readathon
 -- ------------------------------------------------------
--- Server version	5.1.73
+-- Server version	5.5.60-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,12 +23,12 @@ DROP TABLE IF EXISTS `classes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `classes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `gradeid` int(11) DEFAULT NULL,
   `teacherid` int(11) DEFAULT NULL,
   `passwd` char(16) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,6 @@ CREATE TABLE `classes` (
 
 LOCK TABLES `classes` WRITE;
 /*!40000 ALTER TABLE `classes` DISABLE KEYS */;
-INSERT INTO `classes` VALUES (1,2,0,'test'),(2,3,1,'test'),(3,5,2,'test'),(4,5,3,'test');
 /*!40000 ALTER TABLE `classes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -49,13 +48,13 @@ DROP TABLE IF EXISTS `classreading`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `classreading` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `classid` int(11) DEFAULT NULL,
   `readdate` date DEFAULT NULL,
   `num_mins` int(11) DEFAULT NULL,
   `passwd` char(16) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,7 +63,6 @@ CREATE TABLE `classreading` (
 
 LOCK TABLES `classreading` WRITE;
 /*!40000 ALTER TABLE `classreading` DISABLE KEYS */;
-INSERT INTO `classreading` VALUES (1,3,'2016-01-21',70,''),(2,4,'2016-01-28',100,''),(3,4,'2016-01-29',150,''),(4,3,'2016-01-29',150,''),(5,1,'2016-01-22',200,'');
 /*!40000 ALTER TABLE `classreading` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,10 +74,10 @@ DROP TABLE IF EXISTS `grades`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `grades` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `grade` char(8) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,7 +86,7 @@ CREATE TABLE `grades` (
 
 LOCK TABLES `grades` WRITE;
 /*!40000 ALTER TABLE `grades` DISABLE KEYS */;
-INSERT INTO `grades` VALUES (1,'Pre-K'),(2,'Kind.'),(3,'Grade 1'),(4,'Grade 2'),(5,'Grade 3'),(6,'Grade 4'),(7,'Grade 5');
+INSERT INTO `grades` VALUES (-1,'Pre-K'),(0,'Kind.'),(1,'Grade 1'),(2,'Grade 2'),(3,'Grade 3'),(4,'Grade 4'),(5,'Grade 5'),(9,'System');
 /*!40000 ALTER TABLE `grades` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -100,12 +98,12 @@ DROP TABLE IF EXISTS `reading`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `reading` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL ,
   `studentid` int(11) DEFAULT NULL,
   `readdate` date DEFAULT NULL,
   `num_mins` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -114,7 +112,6 @@ CREATE TABLE `reading` (
 
 LOCK TABLES `reading` WRITE;
 /*!40000 ALTER TABLE `reading` DISABLE KEYS */;
-INSERT INTO `reading` VALUES (1,1,'2016-01-21',70),(2,2,'2016-01-26',150),(3,2,'2016-01-27',200),(4,3,'2016-01-25',30),(5,3,'2016-01-24',90),(6,4,'2016-01-28',120),(7,1,'2016-01-21',130),(8,4,'2016-01-27',120);
 /*!40000 ALTER TABLE `reading` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,11 +123,11 @@ DROP TABLE IF EXISTS `students`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `students` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `classid` int(11) DEFAULT NULL,
   `name` char(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -139,7 +136,6 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (1,1,'Naomi Weiler'),(2,3,'Miriam Weiler'),(3,3,'Anna Jooste'),(4,4,'Uma Kirk');
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -163,7 +159,6 @@ CREATE TABLE `teachers` (
 
 LOCK TABLES `teachers` WRITE;
 /*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
-INSERT INTO `teachers` VALUES (0,'K. Fearrington'),(1,'M. Miller'),(2,'M. Valente'),(3,'C. Sullivan');
 /*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -176,4 +171,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-11-22 22:13:25
+-- Dump completed on 2019-01-11  3:16:11
