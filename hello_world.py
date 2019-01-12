bf67ab23295aedb0a8edd09e3dfb7c7c10e6a8b4
Jonathan Miller <jrmiller1999@gmail.com>
2019-01-12 04:13:27 +0000

    minor updates for 2019 to hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 
-import _mysql, sys
+import MySQLdb as mdb, sys
 
 print "Content-type: text/html\n\n"
 
 print "<h1>hello world</h1>"
 
 try: 
-    con = _mysql.connect ('localhost', 'nes_read', 'readingrocks', 'readathon2015')
+    con = mdb.connect ('localhost', 'nes_read', 'r3adingr0cks!', 'readathon')
     con.query("select count(*) from grades")
 
     result = con.use_result()
     print "there are %s grades at NES." % result.fetch_row()[0]
 
-except _mysql.Error, e:
+except mdb.Error, e:
     print "Error %d: %s" % (e.args[0], e.args[1])
     sys.exit(1)
 
