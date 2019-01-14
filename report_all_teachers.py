660bfff6404484d28f1a6951689f24a0c999ccf8
Jonathan Miller <jrmiller1999@gmail.com>
2019-01-14 02:09:31 +0000

    move report_all_teacher.py from updates_2016 to main dir

--- report_all_teachers.py
+++ report_all_teachers.py
@@ -24,6 +24,13 @@ except mdb.Error as e:
     print "Error in query: %s" % grade_q
     sys.exit(1)
 
+try: 
+    classes_q = "select id from classes where gradeid = " + gradeid
+    cursor.execute(classes_q)
+    classes = cursor.fetchall()
+except mdb.Error as e:
+    print "Error in query: %s" % classes_q
+    sys.exit(1)
 
 try: 
     minutes_q = "select "
@@ -31,19 +38,47 @@ try:
     minutes_q +=   "(select id from students where classid in "
     minutes_q +=     "(select id from classes where gradeid = %s) " % gradeid
     minutes_q +=    ")"
-    minutes_q +=  " ) + "
-    minutes_q += "(select sum(num_mins) from classreading where classid in "
-    minutes_q +=   "(select id from classes where gradeid = %s) " % gradeid
-    minutes_q +=  ")" 
+    minutes_q += ")"
+#    minutes_q +=  " ) + "
+#    minutes_q += "(select sum(num_mins) from classreading where classid in "
+#    minutes_q +=   "(select id from classes where gradeid = %s) " % gradeid
+#    minutes_q +=  ")" 
+##    print "query is %s" % minutes_q
     cursor.execute(minutes_q)
     minutes = cursor.fetchone()
 except mdb.Error as e:
     print "Error in query: %s" % minutes_q
     sys.exit(1)
 
+total_min = minutes[0]
 
+for myclass in classes:
+    try: 
+        numstudents_q = "select count(*) from students where classid = %d " % myclass
+        cursor.execute(numstudents_q)
+        numstudents = cursor.fetchone()
+    except mdb.Error as e:
+        print "Error in query: %s" % grade_q
+        sys.exit(1)
+
+    try:
+        classreading_q = "select sum(num_mins) from classreading where classid = %d " % myclass
+        cursor.execute(classreading_q)
+        classreading = cursor.fetchone()
+    except mdb.Error as e:
+        print "Error in query: %s" % grade_q
+        sys.exit(1)
+
+#    print myclass[0]
+    if cursor.rowcount > 0:
+#        print "<br>rows returned %d " % cursor.rowcount
+        classtotal = int(classreading[0]) * int(numstudents[0])
+#        print "classreading %d <br><br> " % classreading
+#        print "numstudents %d <br><br>"%  numstudents
+        total_min += classtotal
     
-print "Total reading time for %s: %d" % (str(grade[0]), minutes[0]) 
+#print "Total reading time for %s: %d" % (str(grade[0]), minutes[0]) 
+print "Total reading time for Grade %s: %d" % (str(grade[0]), total_min) 
 
 
 jrm.print_footer()
